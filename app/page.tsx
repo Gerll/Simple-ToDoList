9cb69672719eb3ea9753b8da3b37780772abb9a8
gerll <gerll.bm@hotmail.com>
2023-08-24 17:03:07 -0700

    Created the component AddTask

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image'
+import AddTask from './components/AddTask'
+import React from 'react'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Next 13</h1>
+    <main className="flex min-h-screen flex-col  justify-between p-24 max-w-4xl mx-auto">
+      <div className='text-center my-5 flex flex-col gap-4'>
+        <h1 className='text-2xl font-bold'>Todo List App</h1>
+        <AddTask />
+      </div>
     </main>
   )
 }
